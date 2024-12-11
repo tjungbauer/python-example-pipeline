a1e16e25091f6e467f596332507a73a198c95f15
Thomas Jungbauer <25078868+tjungbauer@users.noreply.github.com>
2024-12-11 15:54:45 +0100

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+import datetime
+
+print("Hello from Jenkins! Your build is successful. PQ Test has been passed")
+
+# Get current date and time
+current_date_time = datetime.datetime.now()
+
+# Print current date and time
+print("Current date and time:", current_date_time)
